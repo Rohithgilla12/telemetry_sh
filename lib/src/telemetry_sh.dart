6795e99f299daee4fdc0b3a84dbc60205f67a842
Rohith Gilla <gillarohith1@gmail.com>
2024-08-19 18:24:19 +0530

    feat: implement v1 for telemetry sdk

--- lib/src/telemetry_sh.dart
+++ lib/src/telemetry_sh.dart
@@ -1,7 +1,102 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
 /// {@template telemetry_sh}
 /// A simple telemetry logging SDK for Dart
 /// {@endtemplate}
 class TelemetrySh {
   /// {@macro telemetry_sh}
-  const TelemetrySh();
+  TelemetrySh(
+    String apiKey, {
+    http.Client? client,
+  })  : _apiKey = apiKey,
+        _client = client ?? http.Client();
+
+  final String? _apiKey;
+  final http.Client _client;
+  final String _baseUrl = 'https://api.telemetry.sh';
+
+  /// Log data to a specific table
+  ///
+  /// [table] is the name of the table to log data to.
+  /// [data] is a map containing the data to be logged.
+  /// [options] is an optional map of additional parameters for the log request.
+  ///
+  /// Returns a [Future] that completes with a [Map] containing
+  /// the response from the server.
+  ///
+  /// Throws an [Exception] if the API request fails or returns an error.
+  /// Example:
+  ///
+  /// ```dart
+  /// await telemetry.log('users', {
+  ///  'name': 'John Doe',
+  ///  'email': 'john.doe@test.com'
+  /// });
+  /// ```
+  ///
+  Future<Map<String, dynamic>> log(
+    String table,
+    Map<String, dynamic> data, {
+    Map<String, dynamic> options = const {},
+  }) async {
+    final headers = {
+      'Content-Type': 'application/json',
+      'Authorization': _apiKey!,
+    };
+
+    final body = {
+      'data': data,
+      'table': table,
+      ...options,
+    };
+
+    final response = await _client.post(
+      Uri.parse('$_baseUrl/log'),
+      headers: headers,
+      body: jsonEncode(body),
+    );
+
+    return jsonDecode(response.body) as Map<String, dynamic>;
+  }
+
+  /// Query the telemetry data
+  ///
+  /// [query] is the SQL query string to execute.
+  /// [options] is an optional map of additional query parameters.
+  ///
+  /// Returns a [Future] that completes with a [Map] containing
+  ///  the query results.
+  ///
+  /// Throws an [Exception] if the API request fails or returns an error.
+  /// Example:
+  ///
+  /// ```dart
+  /// await telemetry.query('SELECT * FROM users');
+  /// ```
+  Future<Map<String, dynamic>> query(
+    String query, {
+    Map<String, dynamic> options = const {},
+  }) async {
+    final headers = {
+      'Content-Type': 'application/json',
+      'Authorization': _apiKey!,
+    };
+
+    final body = {
+      'query': query,
+      'realtime': true,
+      'json': true,
+      ...options,
+    };
+
+    final response = await _client.post(
+      Uri.parse('$_baseUrl/query'),
+      headers: headers,
+      body: jsonEncode(body),
+    );
+
+    return jsonDecode(response.body) as Map<String, dynamic>;
+  }
 }
--- test/src/telemetry_sh_test.dart
+++ test/src/telemetry_sh_test.dart
@@ -1,11 +1,90 @@
-// ignore_for_file: prefer_const_constructors
+import 'package:http/http.dart' as http;
+import 'package:mocktail/mocktail.dart';
 import 'package:telemetry_sh/telemetry_sh.dart';
 import 'package:test/test.dart';
 
+class MockHttpClient extends Mock implements http.Client {}
+
 void main() {
   group('TelemetrySh', () {
+    late TelemetrySh telemetry;
+    late MockHttpClient mockHttpClient;
+
+    setUp(() {
+      mockHttpClient = MockHttpClient();
+      telemetry = TelemetrySh('test_api_key', client: mockHttpClient);
+    });
+
     test('can be instantiated', () {
-      expect(TelemetrySh(), isNotNull);
+      expect(telemetry, isNotNull);
+    });
+
+    group('log', () {
+      test('sends correct request and returns parsed response', () async {
+        // Arrange
+        final expectedUrl = Uri.parse('https://api.telemetry.sh/log');
+        final expectedHeaders = {
+          'Content-Type': 'application/json',
+          'Authorization': 'test_api_key',
+        };
+
+        final mockResponse = http.Response('{"success": true}', 200);
+
+        when(
+          () => mockHttpClient.post(
+            expectedUrl,
+            headers: expectedHeaders,
+            body: any(named: 'body'),
+          ),
+        ).thenAnswer((_) async => mockResponse);
+
+        // Act
+        final result = await telemetry.log('users', {'name': 'John Doe'});
+
+        // Assert
+        verify(
+          () => mockHttpClient.post(
+            expectedUrl,
+            headers: expectedHeaders,
+            body: any(named: 'body'),
+          ),
+        ).called(1);
+        expect(result, {'success': true});
+      });
+    });
+
+    group('query', () {
+      test('sends correct request and returns parsed response', () async {
+        // Arrange
+        final expectedUrl = Uri.parse('https://api.telemetry.sh/query');
+        final expectedHeaders = {
+          'Content-Type': 'application/json',
+          'Authorization': 'test_api_key',
+        };
+
+        final mockResponse = http.Response('{"results": []}', 200);
+
+        when(
+          () => mockHttpClient.post(
+            expectedUrl,
+            headers: expectedHeaders,
+            body: any(named: 'body'),
+          ),
+        ).thenAnswer((_) async => mockResponse);
+
+        // Act
+        final result = await telemetry.query('SELECT * FROM users');
+
+        // Assert
+        verify(
+          () => mockHttpClient.post(
+            expectedUrl,
+            headers: expectedHeaders,
+            body: any(named: 'body'),
+          ),
+        ).called(1);
+        expect(result, {'results': <List<Map<String, dynamic>>>[]});
+      });
     });
   });
 }
